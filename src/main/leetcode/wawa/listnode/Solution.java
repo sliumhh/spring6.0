83aa11c80d84398313d4903c8bbb68838880c15c
tristanliushuai <tristanliushuai@didiglobal.com>
2021-03-17 21:30:34 +0800

    cg listnode

--- src/main/leetcode/wawa/listnode/Solution.java
+++ src/main/leetcode/wawa/listnode/Solution.java
@@ -11,19 +11,29 @@ public class Solution {
 
     /**
      * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+     *
      * @param head
+     *
      * @return
      */
     public ListNode reverseListNode(ListNode head) {
-        if (head == null) return null;
-        ListNode pre = null, next;
-        while (head != null) {
-            next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
+//        if (head == null) return null;
+//        ListNode pre = null, next;
+//        while (head != null) {
+//            next = head.next;
+//            head.next = pre;
+//            pre = head;
+//            head = next;
+//        }
+//        return pre;
+        if (head == null || head.next == null) {
+            return head;
         }
-        return pre;
+        ListNode newHead = reverseListNode(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+
     }
 
 }
