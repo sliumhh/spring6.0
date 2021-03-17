ac24de2d3f22ba421455eed5e26267d3270f3973
tristanliushuai <tristanliushuai@didiglobal.com>
2021-03-17 21:05:46 +0800

    add listnode

--- src/main/leetcode/liushuai/Offer.java
+++
@@ -1,8 +0,0 @@
-package main.leetcode.liushuai;
-
-/**
- * @author ezrealhexin
- * @date 2021-03-17 20:09
- **/
-public class Offer {
-}
---
+++ src/main/leetcode/wawa/listnode/Solution.java
@@ -0,0 +1,29 @@
+package main.leetcode.wawa.listnode;
+
+/**
+ * 功能描述:
+ *
+ * @author sliu
+ * @date 2021/3/17 8:33 下午
+ */
+public class Solution {
+
+
+    /**
+     * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+     * @param head
+     * @return
+     */
+    public ListNode reverseListNode(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = null, next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+}
