a688f40e42c61626fdd27062024008b10ecb8848
tristanliushuai <tristanliushuai@didiglobal.com>
2021-03-18 16:12:16 +0800

    剑指offer 链表

---
+++ src/main/leetcode/wawa/listnode/Node.java
@@ -0,0 +1,18 @@
+package main.leetcode.wawa.listnode;
+
+/**
+ * 功能描述:
+ *
+ * @author sliu
+ * @date 2021/3/17 8:31 下午
+ */
+public class Node {
+
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+    }
+}
